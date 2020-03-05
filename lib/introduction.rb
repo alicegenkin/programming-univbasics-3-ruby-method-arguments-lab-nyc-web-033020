d625397f5b2a362a6da0b2fa4b2729d6231f4b8f
Alice <agenkin1@gmail.com>
2020-03-05 20:35:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,9 @@
 #Define a method that takes and uses two parameters
 #Define a method with an optional parameter
 
-
+def introduction (name)
+  puts this is a phrase with a #{name}
+end
 def introduction_with_language (name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
